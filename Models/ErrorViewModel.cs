f0aa927c6479883b890c26d2d796b90c5efbba15
Chuck Dascalos <dascalos@hotmail.com>
2024-01-23 14:03:04 -0500

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace dos_test_app.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
